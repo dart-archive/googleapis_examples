086d90f771900968c926d15ab78dd30c1b2c477c
Martin Kustermann <kustermann@google.com>
2014-09-17 14:48:35 +0200

    Add example of using Custom Search in the browser
    R=sgjesse@google.com
    
    Review URL: https://codereview.chromium.org//580803002

--- README.md
+++ README.md
@@ -29,6 +29,30 @@ Cloud Project. The Developers Console can be used to create one via a web
 interface.
 
 
+#### Example: customsearch_browser_apikey
+
+This example demonstrates the usage of the
+[Google Custom Search](https://developers.google.com/custom-search/) API. It
+shows how to search on pub.dartlang.org.
+
+In order to run this sample you need to replace "<please fill in>" in
+`web/index.dart` with the API Key information obtained from the Google
+Developers Console.
+
+More information about how to obtain an API Key can be found on the
+[googleapis_auth]
+(https://github.com/dart-lang/googleapis_auth/blob/master/README.md) repository.
+
+Usage of the example:
+```
+$ pub serve
+Loading source assets...
+Serving tomsearch_browser_apikey on http://localhost:8080
+Build completed successfully
+```
+
+Navigate with a browser to http://localhost:8080 and do a search.
+
 
 #### Example: drive_search_console
 
---
+++ customsearch_browser_apikey/web/index.dart
@@ -0,0 +1,79 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:html';
+
+import 'package:googleapis_auth/auth_browser.dart' as auth;
+import 'package:googleapis/customsearch/v1.dart' as search;
+
+// This is the pub custom search ID - available at:
+// https://github.com/dart-lang/pub-dartlang/blob/master/app/handlers/search.py
+final customSearchId = '009011925481577436976:h931xn2j7o0';
+
+// This is the API Key obtained from the Google Developers Console.
+final apiKey = 'AIzaSyCQK0Z1nSTf3PCVUEz4rX5CKjLqaP4gtmE';
+
+class Package {
+  final String title;
+  final String url;
+  final String snippet;
+
+  Package(this.title, this.url, this.snippet);
+}
+
+// Use the Custom Search API to search for pub packages.
+Future<List<Package>> searchPackages(search.CustomsearchApi api, String query) {
+  return api.cse.list(query, cx: customSearchId).then((search.Search search) {
+    var packages = [];
+    if (search.items != null) {
+      for (var result in search.items) {
+        packages.add(
+            new Package(result.htmlTitle, result.link, result.htmlSnippet));
+      }
+    }
+    return packages;
+  });
+}
+
+main() {
+  InputElement searchText = querySelector('#search_text');
+  ButtonElement searchButton = querySelector('#search_button');
+  DivElement results = querySelector('#results');
+
+  var client = auth.clientViaApiKey(apiKey);
+  var api = new search.CustomsearchApi(client);
+
+  searchText.onInput.listen((_) {
+    searchButton.disabled = searchText.value == '';
+  });
+
+  searchButton.onClick.listen((_) {
+    searchPackages(api, searchText.value).then((List<Package> packages) {
+      results.children.clear();
+
+      if (packages.isEmpty){
+        display('<h5>No results found.</h5>', results);
+      } else {
+        for (var package in packages) {
+          display('<h3><a href="${package.url}">${package.title}</a></h3>',
+                  results);
+          display(package.snippet, results);
+        }
+      }
+    });
+  });
+}
+
+// Adds a new <div>$msg</div> as a child node of `parent`.
+void display(msg, parent) {
+  var div = new DivElement();
+  div.setInnerHtml('$msg', treeSanitizer: NullTreeSanitizer.Instance);
+  parent.children.add(div);
+}
+
+class NullTreeSanitizer implements NodeTreeSanitizer {
+  static final Instance = new NullTreeSanitizer();
+  void sanitizeTree(node) {}
+}
